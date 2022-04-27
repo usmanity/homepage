8cb2aaf0531f6b2fe16f0875b269b9d5faf52675
usmanity <contact@mhmd.us>
2022-04-27 16:44:13 -0700

    deps: adding tailwind

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
+
+const pages = [
+  {
+    link: "/about",
+    title: "About",
+  },
+  {
+    link: "https://blog.usmanity.com",
+    title: "Blog",
+  },
+  {
+    title: "Home",
+    link: "/",
+  },
+];
+
 const Bar = () => {
-  return <>This is a bar element;</>;
+  return (
+    <div className="border border-slate-100 mx-10 my-6 rounded-3xl py-4 px-8 shadow-sm">
+      <div className="flex">
+        {pages.map((page) => {
+          return (
+            <div className="mr-2" onClick={(page) => {}}>
+              <Link href={page.link}>{page.title}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Bar;
