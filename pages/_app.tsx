097fdaec963ff198d5bdbc9ec793149a08fd89ec
usmanity <contact@mhmd.us>
2022-06-17 22:52:59 -0700

    add tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
+
 function Homepage({ Component, pageProps }: AppProps) {
+  usePanelbear("ELPQ56PqXy5");
   return <Component {...pageProps} />;
 }
 
