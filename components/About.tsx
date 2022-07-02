13d85ce555c52fafd823c24d8ff6ce08cd5b55d0
usmanity <contact@mhmd.us>
2022-07-02 09:49:20 -0700

    wip saving

---
+++ components/About.tsx
@@ -0,0 +1,19 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <div>
+        <div>Name</div>
+        <div>Muhammad Usman</div>
+      </div>
+      <div>
+        <div>Occupation</div>
+        <div>Software Engineer</div>
+      </div>
+      <div>
+        <div>Company</div>
+        <div>Square &dash; Block</div>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,45 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { About } from "../components/About";
+import { Blog } from "../components/Blog";
+import { Contact } from "../components/Contact";
 
-const Home: NextPage = () => {
+export async function getServerSideProps() {
+  const response = await fetch("https://blog.usmanity.com/feed/json");
+  const jsonResponse = await response.json();
+  return {
+    props: {
+      blog: jsonResponse,
+    },
+  };
+}
+
+const Home: NextPage = ({ blog }) => {
+  const [hidden, setHidden] = useState(false);
+  console.log({
+    blog,
+    items: blog.items,
+  });
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const lsHelloValue = localStorage.getItem("hideHello");
+      if (lsHelloValue) {
+        setHidden(true);
+      } else {
+        setTimeout(() => {
+          localStorage.setItem("hideHello", "true");
+          setHidden(true);
+        }, 4000);
+      }
+    }
+  });
+
   return (
     <div
       className="
       flex flex-col h-screen max-h-min
+      motion-reduce:transition-none motion-reduce:hover:transform-none
     "
     >
       <Head>
@@ -19,18 +53,35 @@ const Home: NextPage = () => {
         ></script>
       </Head>
 
-      <main className="h-full">
+      <main className="h-full transition-all">
         <div
           className="flex flex-col h-full justify-center text-center
         bg-white dark:bg-black dark:text-white
         "
         >
-          <h1 className="flex justify-center font-bold animate-[wave] text-3xl">
-            <span className="font-emoji transition-transform animate-[wave_3s_ease-in]">
-              👋
-            </span>{" "}
-            Hello.
-          </h1>
+          <div
+            className={`${
+              hidden ? "hidden" : ""
+            } h-full transition-all animate-[fadeOut_4s_ease-out] delay-1000`}
+          >
+            <h1 className="flex justify-center font-bold animate-[wave] text-3xl">
+              <span className="font-emoji transition-transform animate-[wave_3s_ease-in]">
+                👋
+              </span>{" "}
+              Hello.
+            </h1>
+          </div>
+          <div className={`${!hidden ? "hidden" : ""} h-full transition-all`}>
+            <About />
+            <Contact />
+            <Blog />
+          </div>
+          {/* <div>
+            {blog?.items &&
+              blog.items.map((item: any) => {
+                return <p>{item.title}</p>;
+              })}
+          </div> */}
         </div>
       </main>
     </div>
