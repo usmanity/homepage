0c6d8cc98c41ad3680be8b7f0f13581ff1c6ea4c
usmanity <contact@mhmd.us>
2022-07-07 21:56:50 -0700

    add borders

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,18 @@
 export const About = () => {
   return (
-    <div className="border md:w-1/3 my-0 w-full my-2 md:m-auto md:mr-2 align-top">
-      <h1 className="border-b">About</h1>
-      <div className="flex">
+    <div className="border m-auto md:w-1/3 md:mr-2 md:mt-0 w-full align-top">
+      <div className="flex border-b dark:bg-slate-800 h-8 items-center">
+        <div className="w-full">About</div>
+      </div>
+      <div className="flex border-b h-8 items-center">
         <div className="w-2/5 text-left pl-2">Name</div>
         <div>Muhammad Usman</div>
       </div>
-      <div className="flex">
+      <div className="flex border-b h-8 items-center">
         <div className="w-2/5 text-left pl-2">Occupation</div>
         <div>Software Engineer</div>
       </div>
-      <div className="flex">
+      <div className="flex h-8 items-center">
         <div className="w-2/5 text-left pl-2">Company</div>
         <div>Square - Block</div>
       </div>
--- components/Blog.tsx
+++ components/Blog.tsx
@@ -2,12 +2,15 @@
 export const Blog = ({ posts }) => {
   return (
     <div className="border md:my-0 md:m-auto my-2 md:w-2/3 w-full">
-      <h1 className="border-b">Recent blog posts</h1>
+      <div className="flex border-b dark:bg-slate-800 h-8 items-center">
+        <div className="w-full">Recent blog posts</div>
+      </div>
       <div>
         {posts.map((post: any) => {
           return (
             <div
-              className="flex text-left hover:cursor-pointer hover:opacity-50 transition-opacity"
+              className="flex text-left hover:cursor-pointer hover:opacity-50 transition-opacity
+               border-b dark:border-gray-400 h-8 items-center last-of-type:border-0"
               key={post.id}
               onClick={() => {
                 // @ts-ignore
