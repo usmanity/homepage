bb1eba457f0a4bf527b2f7d3db19c4ba2df1d480
Muhammad Usman <contact@mhmd.us>
2023-10-26 12:34:20 -0700

    using a new build process

--- src/lib/bento/LogoView.svelte
+++ src/lib/bento/LogoView.svelte
@@ -1,3 +1,3 @@
 <div class="LogoView w-full h-full rounded-3xl overflow-hidden">
-  <img src="/mu2.png" class="w-full aspect-square rounded-3xl" alt="a logo image of MU"/>
+  <img src="/mu2.png" class="w-full aspect-square rounded-3xl" alt="MU"/>
 </div>
