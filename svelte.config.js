0fb3befb20992f0ff95fbe440d764eb16a354621
Muhammad Usman <contact@mhmd.us>
2023-10-26 13:02:07 -0700

    using the bikes config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,19 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "@sveltejs/adapter-static";
+const dev = process.argv.includes("dev");
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    adapter: adapter({
+      strict: false,
+    }),
+    paths: {
+      base: "",
+    },
+  },
+};
+
+export default config;
