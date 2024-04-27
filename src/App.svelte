174191b1868d5aacd1647c715adda037a8b4d50d
Muhammad Usman <usmanity@duck.com>
2024-04-27 09:49:19 -0700

    adding downloads icons

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import { Route, Router } from "svelte-routing";
   import MainContent from "./lib/MainContent.svelte";
-
+  import Downloads from "./lib/downloads/Downloads.svelte";
+  import Dots from "./lib/projects/Dots.svelte";
+  import Stfuh from "./lib/projects/STFUH.svelte";
+  export let url = "";
 </script>
 
-<main class="bg-gray-100 min-h-screen dark:bg-stone-900">
-  <MainContent />
-</main>
+<Router {url}>
+  <main class="bg-gray-100 min-h-screen dark:bg-stone-900 dark:text-white">
+    <Route path="/downloads" component={Downloads} />
+    <Route path="/projects/dots" component={Dots} />
+    <Route path="/projects/STFUH" component={Stfuh} />
+    <Route path="/"><MainContent /></Route>
+  </main>
+</Router>
 
 <style>
-
 </style>
---
+++ src/lib/projects/STFUH.svelte
@@ -0,0 +1,5 @@
+<div
+  class="text-center text-[320px] items-center flex leading-[260px] p-12 font-[inter] font-bold tracking-tighter w-screen h-screen text-white bg-black"
+>
+  Shut the fuck up hoe
+</div>
