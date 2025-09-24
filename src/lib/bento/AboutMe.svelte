168f4692ebd60716575a5c83c9cfce71ab2b67e7
Muhammad Usman <muhammad@instrumentl.com>
2025-09-23 17:10:35 -0700

    left job

--- src/lib/bento/AboutMe.svelte
+++ src/lib/bento/AboutMe.svelte
@@ -5,10 +5,6 @@ dark:bg-zinc-800 dark:border dark:border-zinc-700">
   </h2>
   <p class="font-medium text-md text-gray-800 dark:text-neutral-100 mt-1">
     I'm Muhammad Usman. <br/>I currently live in Portland, Oregon.</p>
-  <p class="font-medium text-md text-gray-800 dark:text-neutral-100">
-    These days, I work as a web developer at <a class="text-indigo-800 dark:text-yellow-200 hover:decoration-1 
-    hover:underline hover:decoration-solid hover:text-indigo-600 dark:hover:text-yellow-300" 
-    href="https://numeralhq.com/?ref=usmanity.com">Numeral</a>.</p>
   <img src="/computer-guy-3.png" class="rounded-xl mt-4 dark:brightness-50 
   dark:hover:brightness-90 transition-all duration-1000 ease-in-out" alt="person using computer"/>
 </div>
