67b983b749f14d21ba829315f419287c4c55b506
Muhammad Usman <muhammad@instrumentl.com>
2026-01-09 11:00:42 -0800

    update for 2026

--- src/lib/projects/STFU.svelte
+++
@@ -1,5 +0,0 @@
-<div
-  class="uppercase font-extrabold text-center text-[320px] items-center flex tracking-loose leading-[360px] p-12 font-[inter] w-screen h-screen text-white bg-black"
->
-  Shut the fuck up
-</div>
